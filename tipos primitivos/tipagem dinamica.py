a39eee4623d3fe9f016524fd9dea21e31029d605
Bruno Henrique Alves Santos <bhas795001@gmail.com>
2024-08-16 18:14:08 +0000

    comandos iniciais pythom.

---
+++ tipos primitivos/tipagem dinamica.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls|| clear")#tipagem dinamica
+
+
+nome = "marta"
+idade = 22
+altura = 1.65
+
+print(f"seu nome: {nome} " )
+print(f"idade: {idade} " )
+print(f"altura: {altura}")
---
+++ tipos primitivos/tipagem estatica.py
@@ -0,0 +1,13 @@
+#tipagem estática
+
+nome : str #= cadeia
+idade : int # = inteiro
+altura: float 
+
+nome = "marta"
+idade = 20
+altura = 1.65
+
+print(f"seu nome: {nome} " )
+print(f"idade: {idade}" )
+print(f"altura: {altura}")
